6629bb91e9266d6ce46158dc7dc480b3ed37373b
voomsuetam <mateusmoov@gmail.com>
2024-08-10 01:32:58 -0300

    feat: navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { User } from "lucide-react";
+
+function Navbar() {
+  return (
+    <nav className="w-full border-b py-5 border-dark-gray">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center gap-x-3">
+          <User width="24" className="text-medium-gray" />
+          <p className="text-medium-gray text-xl font-bold">Twitch Profile</p>
+        </div>
+
+        <div className="flex items-center gap-x-8">
+          <a
+            href="#"
+            className="px-4 py-3 rounded-lg border text-white border-dark-gray"
+          >
+            Inicio
+          </a>
+          <a href="#" className="px-4 py-3 text-white border-dark-gray">
+            Como usar?
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="#"
+            className="px-8 py-3 bg-purple text-white font-bold rounded"
+          >
+            Entrar em contato
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "light-gray": "#FDFDFD",
+        "medium-gray": "#9C9C9C",
+        "dark-gray": "#2C2C2D",
+        "almost-black": "#0D0D0E",
+        "off-white": "#F5F5FF",
+        black: "#09090A",
+        purple: "#9747FF",
+        white: "#FFFFFF",
+      },
+      backgroundImage: {
+        "gradient-green": "linear-gradient(#03AD00, #0FC10C)",
+        "gradient-dark": "linear-gradient(#09090A, #09090A00 0%)",
+        "gradient-blue": "linear-gradient(#208BEE, #329CFF)",
+        "gradient-purple": "linear-gradient(#AA58FF, #7A2DDD)",
+        "gradient-orange": "linear-gradient(#FF470F, #FF6330)",
+      },
+    },
   },
   plugins: [],
 };
